75e3f273f6eb20c60420b4655323b01980954e8c
cwcarrillov <cwcarrillov@outlook.es>
2026-09-19 11:27:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import librería_funciones as lf
 
 st.title("Paradigmas de la programación")
 
@@ -7,3 +8,10 @@ st.sidebar.image("UCG.png")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Carlos Carrillo")
+
+capital = st.number_input("Ingrese el capital")
+tasa_anual_pct = st.number_input("Ingrese de la tasa anual")
+dias_mora = st.number_input("Ingrese los días de mora")
+
+
+resultado = lf.calcular_interes_mora()
